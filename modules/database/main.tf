6168b9a8c9e87a6539b29755e4e70784f17356f0
mstanislawski <mstanislawski@cloudferro.com>
2025-06-15 11:35:26 +0200

    update

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -30,7 +30,7 @@ resource "google_sql_database_instance" "postgres" {
     }
 
     ip_configuration {
-      ipv4_enabled                                  = true
+      ipv4_enabled                                  = false
       private_network                               = var.vpc_network_id
       enable_private_path_for_google_cloud_services = true
       ssl_mode                                      = "ENCRYPTED_ONLY"
